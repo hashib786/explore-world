56ee986a284e6e6a29c62f9c8f63dcb942843d3a
Hashib Raja <hashibraja23@gmail.com>
2023-08-09 20:21:35 +0530

    implemented monthly plan aggregation

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -149,3 +149,53 @@ export const getTourStats = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getMonthlyPlan = async (req: Request, res: Response) => {
+  try {
+    const year: number = +req.params.year;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: { plan },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      error: error,
+    });
+  }
+};
--- src/routes/tourRoutes.ts
+++ src/routes/tourRoutes.ts
@@ -4,6 +4,7 @@ import {
   createTour,
   deleteTour,
   getAllTour,
+  getMonthlyPlan,
   getTour,
   getTourStats,
   updateTour,
@@ -14,6 +15,7 @@ const router = express.Router();
 // router.param("id", checkId);
 
 router.route("/tour-stats").get(getTourStats);
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/top-5-tour").get(aliasTopTour, getAllTour);
 router.route("/").get(getAllTour).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
