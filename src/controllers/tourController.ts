3a1fa57521cf79cc1c65ce4465d931e7d6226f24
Hashib Raja <hashibraja23@gmail.com>
2023-08-09 17:08:16 +0530

    remove unnecessary comments

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -132,23 +132,6 @@ export const getTourStats = async (req: Request, res: Response) => {
       },
     ]);
 
-    // const stats = await Tour.aggregate([
-    //   {
-    //     $match: { ratingsAverage: { $gte: 4.5 } },
-    //   },
-    //   {
-    //     $group: {
-    //       _id: null,
-    //       numTours: { $sum: 1 },
-    //       numRatings: { $sum: "$ratingsQuantity" },
-    //       avgRating: { $avg: "$ratingsAverage" },
-    //       avgPrice: { $avg: "$price" },
-    //       minPrice: { $min: "$price" },
-    //       maxPrice: { $max: "$price" },
-    //     },
-    //   },
-    // ]);
-
     res.status(200).json({
       status: "success",
       data: { stats },
