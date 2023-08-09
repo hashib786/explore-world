067b3ea226ecbacbcaa5b37f02eaeba153178d5f
Hashib Raja <hashibraja23@gmail.com>
2023-08-09 20:34:05 +0530

    added comments in aggregation pipline important

--- src/controllers/tourController.ts
+++ src/controllers/tourController.ts
@@ -118,21 +118,32 @@ export const deleteTour = async (req: Request, res: Response) => {
 export const getTourStats = async (req: Request, res: Response) => {
   try {
     const stats = await Tour.aggregate([
+      // in match you can filter and then you can do anything
       { $match: { price: { $gte: 1000 } } },
+      // grouping given functionality so whichone you filtered you perfomed some action in grouping
       {
         $group: {
+          // _id give you sepration like here all group seprated by difficulty --> easy, medium, difficult
+          // $toUpper --> convert to uppercase string
           _id: { $toUpper: "$difficulty" },
+          // $sum --> for counting anythng like eg : 1 --> added only one
+          // $sum --> for counting $ratingsQuantity field and that value
           numTours: { $sum: 1 },
           numRatings: { $sum: "$ratingsQuantity" },
+          // $avg --> this is for avarage
           avgRating: { $avg: "$ratingsAverage" },
           avgPrice: { $avg: "$price" },
+          // $min --> finding minimum
           minPrice: { $min: "$price" },
+          // $max --> finding maximum
           maxPrice: { $max: "$price" },
         },
       },
       {
+        // $sort --> after grouping it basically sort avgPrice(this is defined in grouping) --> 1 mean assending 0 mean descending
         $sort: { avgPrice: 1 },
       },
+      // you can add another filter also like $match
       // {
       //   $match: { _id: { $ne: 'EASY' } }
       // }
@@ -155,11 +166,15 @@ export const getMonthlyPlan = async (req: Request, res: Response) => {
     const year: number = +req.params.year;
     const plan = await Tour.aggregate([
       {
+        // $unwind --> this unwind basically it spread array like in one document have array [1, 2, 3] so
+        // unwind seprate in seprate num like 1, 2 etc.
         $unwind: "$startDates",
       },
       {
+        // $match --> here after of unwinding there added filtering
         $match: {
           startDates: {
+            // $gte --> gte is also working in date so here select date is gte given year and lte also doing like that
             $gte: new Date(`${year}-01-01`),
             $lte: new Date(`${year}-12-31`),
           },
@@ -169,13 +184,16 @@ export const getMonthlyPlan = async (req: Request, res: Response) => {
         $group: {
           _id: { $month: "$startDates" },
           numTourStarts: { $sum: 1 },
+          // $push --> basically grouping time push for return array
           tours: { $push: "$name" },
         },
       },
       {
+        // $addFields --> basically add after groping another field like there copy id value in month and return
         $addFields: { month: "$_id" },
       },
       {
+        // $project --> in project if i give _id --> 0 so it hide returning time
         $project: {
           _id: 0,
         },
@@ -184,6 +202,7 @@ export const getMonthlyPlan = async (req: Request, res: Response) => {
         $sort: { numTourStarts: -1 },
       },
       {
+        // $limit --> limit basically give you only given number value
         $limit: 12,
       },
     ]);
