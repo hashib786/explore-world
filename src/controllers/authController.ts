4ec9bb25b37272ceaef790a4a817388319692089
Hashib Raja <hashibraja23@gmail.com>
2023-08-12 21:13:09 +0530

    downloaded jwt and also send toke when user is signup

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/cathAsync";
 import User from "../models/userModel";
+import jwt from "jsonwebtoken";
 
 export const signUp = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      photo: req.body.photo,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET!, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
     res.status(201).json({
       status: "success",
+      token,
       data: {
         newUser,
       },
