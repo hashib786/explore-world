53c90b1bcb037758399217467421472864f3780f
Hashib Raja <hashibraja23@gmail.com>
2023-08-14 20:48:35 +0530

    implemented helmet package and also in app.ts arrange all imports and also added comments before all middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet"; // The helmet package for Node.js adds vital security-related HTTP headers to your application automatically, fortifying it against prevalent web vulnerabilities.
 
 import tourRouter from "./routes/tourRoutes";
 import userRouter from "./routes/userRoutes";
@@ -11,28 +12,34 @@ import unhandledRoute from "./controllers/404route";
 
 const app = express();
 
-// This is for how many requests one IP address can request
+// Apply helmet middleware for enhanced security
+app.use(helmet());
+
+// Apply rate limiting middleware for IP-based request limits
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
   max: 100,
   message: "Too many requests from the IP, please try again in one hour",
 });
-
 app.use("/api", limiter);
 
-// Middlware
-app.use(express.json());
+// Middleware for parsing JSON data and setting a size limit
+app.use(express.json({ limit: "10kb" }));
+
+// Middleware for logging requests in development mode
 process.env.NODE_ENV === "development" && app.use(morgan("dev"));
+
+// Middleware to serve static files from the "public" directory
 app.use(express.static(join(currentWorkingDirectory, "public")));
 
-// Routes
+// Routes for tours and users
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-// Unhadled Routes
+// Unhandled Routes
 app.all("*", unhandledRoute);
 
-// handle globel error handling
+// Global error handling middleware
 app.use(errorController);
 
 export default app;
