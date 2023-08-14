6468290fa5a262ae6a54593449993d495db00928
Hashib Raja <hashibraja23@gmail.com>
2023-08-14 21:02:04 +0530

    added middleware for security for save NoSQL query injection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet"; // The helmet package for Node.js adds vital security-related HTTP headers to your application automatically, fortifying it against prevalent web vulnerabilities.
+import mongoSantize from "express-mongo-sanitize";
 
 import tourRouter from "./routes/tourRoutes";
 import userRouter from "./routes/userRoutes";
@@ -25,6 +26,10 @@ app.use("/api", limiter);
 
 // Middleware for parsing JSON data and setting a size limit
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization againts NoSql query injection
+app.use(mongoSantize());
+// Date sanitization againts XSS
 
 // Middleware for logging requests in development mode
 process.env.NODE_ENV === "development" && app.use(morgan("dev"));
