a548c5699996452e66a9c693de4e8c6a555c908f
Hashib Raja <hashibraja23@gmail.com>
2023-08-19 17:36:36 +0530

    serving overview card pages static

--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
+import Tour from "../models/tourModel";
 
 export const getOverview = (req: Request, res: Response) => {
+  const tours = Tour.find();
   res.status(200).render("overview", {
     tittle: "This is overview Page",
+    tours,
   });
 };
 
