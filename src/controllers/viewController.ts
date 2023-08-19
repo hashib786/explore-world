1b757fa0fdc2663ae96baecd1b943cbe8ad26b2a
Hashib Raja <hashibraja23@gmail.com>
2023-08-19 18:17:47 +0530

    fill dynamic content in overview cards

--- src/controllers/viewController.ts
+++ src/controllers/viewController.ts
@@ -1,13 +1,16 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Tour from "../models/tourModel";
+import catchAsync from "../utils/cathAsync";
 
-export const getOverview = (req: Request, res: Response) => {
-  const tours = Tour.find();
-  res.status(200).render("overview", {
-    tittle: "This is overview Page",
-    tours,
-  });
-};
+export const getOverview = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const tours = await Tour.find();
+    res.status(200).render("overview", {
+      tittle: "This is overview Page",
+      tours,
+    });
+  }
+);
 
 export const getTourView = (req: Request, res: Response) => {
   res.status(200).render("tour", {
