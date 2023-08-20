f3a4ad2b83e89630ab978f0daab98ce44513572e
Hashib Raja <hashibraja23@gmail.com>
2023-08-20 08:21:47 +0530

    added mapbox our tour detail page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,2 +1,49 @@
-const fddsdsafsa = JSON.parse(document.getElementById("map").dataset.locations);
-console.log(fddsdsafsa);
+const locations = JSON.parse(document.getElementById("map").dataset.locations);
+console.log(locations);
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGFzaGlicmFqYTc4NiIsImEiOiJjbGxpdGJrcWMxOWh3M2RxanEwZWV2c3B0In0.n1hEDv-dvwIW_SVFQUtRCg";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/hashibraja786/cllitp3ap018401pl8yss8dgj",
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745],
+  // zoom: 10,
+  // interactive: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement("div");
+  el.className = "marker";
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: "bottom",
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
